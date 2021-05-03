3cf69c4c2ea90b378d7c81d08fada97735c6bd5d
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2021-05-03 15:52:26 -0700

    background signal kinda works, prints on : though

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-#include <fcntl.h>    // open?
-#include <limits.h>   // PATH_MAX
+#include <fcntl.h>   // open?
+#include <limits.h>  // PATH_MAX
+#include <signal.h>
 #include <stdbool.h>  // bool
 #include <stdio.h>    // printf, fflush
 #include <stdlib.h>   // exit
@@ -22,6 +23,7 @@ struct commandline {
 
 // struct commandline *currLine;
 int lastExitStatus = 0;
+int statusOrSignal = 0;
 int smallshPid;
 
 // functions declarations
@@ -29,14 +31,47 @@ struct commandline *getInput(char *);
 struct commandline *createCommandline(char *);
 void destroyCommandline(struct commandline *);
 void changeDirectory(struct commandline *);
-void printExitStatus(bool);
+void printExitStatus();
 void otherCommands(struct commandline *);
 
-int main(void) {
-  char *line = malloc(MAX_CMD_LINE * sizeof(char));
-  struct commandline *currLine = getInput(line);
-  smallshPid = getpid();
+void handleCtrlC(int signal) {
+  char msg[32];
+  sprintf(msg, "terminated by signal %d\n", signal);
+  write(STDOUT_FILENO, msg, 32);
+}
 
+void childSignalHandler() {
+  // get and display exit status for child
+  int childStatus;
+  pid_t childPid = waitpid(-1, &childStatus, WNOHANG);
+  if (childPid > 0) {
+    char msg[64];
+    memset(msg, '\0', 64);
+    if (WIFEXITED(childStatus)) {
+      sprintf(msg, "background pid %d is done: exit value %d\n", childPid,
+              WEXITSTATUS(childStatus));
+      write(STDOUT_FILENO, msg, 64);
+    } else {
+      sprintf(msg, "background pid %d is done: terminated by signal %d\n",
+              childPid, WTERMSIG(childStatus));
+      write(STDOUT_FILENO, msg, 64);
+    }
+  }
+}
+
+int main(void) {
+  smallshPid = getpid();
+  char *line = malloc(MAX_CMD_LINE * sizeof(char));
+
+  // signals
+  struct sigaction sa;
+  // sa.sa_handler = handler;
+  // sigaction(SIGINT, &sa, NULL);
+
+  // handle ctrl+c
+  signal(SIGINT, handleCtrlC);
+  // get input
+  struct commandline *currLine = getInput(line);
   // command
   while (currLine == NULL || strcmp(currLine->command, "exit") != 0) {
     if (currLine == NULL || strcmp(currLine->command, "#") == 0) {
@@ -47,7 +82,7 @@ int main(void) {
       changeDirectory(currLine);
     } else if (strcmp(currLine->command, "status") == 0) {
       // status
-      printExitStatus(currLine->background);
+      printExitStatus();
     } else {
       // must fork
       otherCommands(currLine);
@@ -57,7 +92,7 @@ int main(void) {
   // "exit" MUST KILL ALL PROCESSES AND JOBS BEFORE TERMINATING
   // exploration process api creating and terminating processes
   // exit() function
-  // 17
+  // 30
   free(line);
   destroyCommandline(currLine);
   return 0;
@@ -74,8 +109,9 @@ struct commandline *createCommandline(char *line) {
   }
 
   // first token is command
-  currLine->command = calloc(strlen(token) + 1, sizeof(char));
-  strcpy(currLine->command, token);
+  currLine->command = strdup(token);
+  // currLine->command = calloc(strlen(token) + 1, sizeof(char));
+  // strcpy(currLine->command, token);
 
   // initialize struct parts
   currLine->args = calloc(MAX_ARGS, sizeof(char *));
@@ -88,18 +124,59 @@ struct commandline *createCommandline(char *line) {
     if (strcmp(token, "<") == 0) {
       // next token is input file
       token = strtok_r(NULL, " ", &saveptr);
-      currLine->redirectInput = calloc(strlen(token) + 1, sizeof(char));
-      strcpy(currLine->redirectInput, token);
+      currLine->redirectInput = strdup(token);
+      // currLine->redirectInput = calloc(strlen(token) + 1, sizeof(char));
+      // strcpy(currLine->redirectInput, token);
     } else if (strcmp(token, ">") == 0) {
       // next token is output file
       token = strtok_r(NULL, " ", &saveptr);
-      currLine->redirectOutput = calloc(strlen(token) + 1, sizeof(char));
-      strcpy(currLine->redirectOutput, token);
+      currLine->redirectOutput = strdup(token);
+      // currLine->redirectOutput = calloc(strlen(token) + 1, sizeof(char));
+      // strcpy(currLine->redirectOutput, token);
     } else {
       // is an arg
-      currLine->args[currLine->argsCount++] = token;
+      if (strstr(token, "$$") != NULL) {
+        // variable expansion $$
+        char *remainingStr = token;
+        // turn smallshPid into a string
+        char smallshPidStr[16];
+        sprintf(smallshPidStr, "%d", smallshPid);
+        // set up space for new expanded string
+        char expanded[MAX_CMD_LINE];
+        memset(expanded, '\0', MAX_CMD_LINE * sizeof(char));
+        int remainingLen = strlen(remainingStr);
+        char nextTwo[3];
+        char nextLetter[2];
+        while (remainingStr != NULL && remainingLen > 0) {
+          if (remainingLen >= 2) {
+            // get next two letters
+            memset(nextTwo, '\0', 3 * sizeof(char));
+            strncpy(nextTwo, remainingStr, 2);
+            if (strcmp(nextTwo, "$$") == 0) {
+              // next two letters are $$
+              strcat(expanded, smallshPidStr);  // append smallshPid
+              remainingStr = &remainingStr[2];  // set pointer to after $$
+              remainingLen = strlen(remainingStr);
+              continue;
+            }
+          }
+          // next two letters are not $$
+          if (remainingLen >= 1) {
+            // get next letter
+            memset(nextLetter, '\0', 2 * sizeof(char));
+            strncpy(nextLetter, remainingStr, 1);
+            strcat(expanded, nextLetter);     // append next 1 letter
+            remainingStr = &remainingStr[1];  // advance pointer
+            remainingLen = strlen(remainingStr);
+          }
+        }
+        currLine->args[currLine->argsCount++] = strdup(expanded);
+      } else {
+        currLine->args[currLine->argsCount++] = strdup(token);
+      }
     }
   }
+
   // check last arg for &: store as boolean and remove from args
   if (currLine->argsCount > 0 &&
       strcmp(currLine->args[currLine->argsCount - 1], "&") == 0) {
@@ -147,44 +224,49 @@ struct commandline *getInput(char *line) {
 void changeDirectory(struct commandline *currLine) {
   if (currLine->args == NULL || currLine->argsCount == 0) {
     // home directory
-    char *homeDir = getenv("HOME");
     chdir(getenv("HOME"));
-    printf("homeDir:%s\n", homeDir);
     fflush(stdout);
   } else {
     char *destDir = currLine->args[0];
-    printf("destDir:%s\n", destDir);
-    fflush(stdout);
-    // handle relative and absolute paths
-    // getcwd returns absolute pathname with null term string
-    // char cwd[PATH_MAX];
     char *cwd = getcwd(NULL, PATH_MAX);
     if (cwd == NULL) {
       perror("getcwd()");
       exit(1);
     }
-    // if destDir includes cwd, it is absolute path
-    if (strstr(destDir, cwd) != NULL) {
-      chdir(destDir);
-    } else {
-      char absolutePath[PATH_MAX];
-      sprintf(absolutePath, "%s/%s", cwd, destDir);
-      chdir(absolutePath);
-    }
+    chdir(destDir);
     free(cwd);
   }
 }
 
-void printExitStatus(bool background) {
-  // print either exit status or terminating signal of last foreground process
-  // ran by shell exit, cd, status do not count as foreground processes (ie
-  // status should ignore build-in commands)
-  if (background) {
-    printf("terminated by signal %d\n", lastExitStatus);
+void printExitStatus() {
+  // print either exit status or terminating signal of last foreground
+  // process ran by shell exit, cd, status do not count as foreground
+  // processes (ie status should ignore build-in commands)
+  if (statusOrSignal == 0) {
+    printf("exit value %d\n", lastExitStatus);  // status
   } else {
-    printf("exit value %d\n", lastExitStatus);
+    printf("terminated by signal %d\n", lastExitStatus);  // signal
   }
   fflush(stdout);
+}
+
+void backgroundPidExit(int *childStatus) {
+  pid_t childPid = 0;
+  while (childPid <= 0) {
+    childPid = waitpid(-1, childStatus, WNOHANG);
+  }
+
+  char msg[64];
+  memset(msg, '\0', 64);
+  if (WIFEXITED(childStatus)) {
+    sprintf(msg, "background pid %d is done: exit value %d\n", childPid,
+            WEXITSTATUS(childStatus));
+    write(STDOUT_FILENO, msg, 64);
+  } else {
+    sprintf(msg, "background pid %d is done: terminated by signal %d\n",
+            childPid, WTERMSIG(&childStatus));
+    write(STDOUT_FILENO, msg, 64);
+  }
 }
 
 void otherCommands(struct commandline *currLine) {
@@ -194,7 +276,8 @@ void otherCommands(struct commandline *currLine) {
   // for command to complete. parent must return command line access to use
   // immediately after forking
 
-  // copy args to newargv for execvp: +1 for command and +1 for NULL ptr term
+  // copy args to newargv for execvp: +1 for command and +1 for NULL ptr
+  // term
   int newargvLen = currLine->argsCount + 2;
   char *newargv[newargvLen];
   newargv[0] = currLine->command;  // first is command
@@ -204,8 +287,16 @@ void otherCommands(struct commandline *currLine) {
   }
 
   // fork a new process
-  int childStatus;
+  if (currLine->background) {
+    // let parent process know when child process finishes
+    signal(SIGCHLD, childSignalHandler);
+  }
+  /* else {
+     signal(SIGCHLD, SIG_IGN);
+   }*/
+
   pid_t childPid = fork();
+  int childStatus;
 
   switch (childPid) {
     case -1:
@@ -213,13 +304,17 @@ void otherCommands(struct commandline *currLine) {
       break;
     case 0:
       // child process executes this branch
+      // dup2 stuff canvas module 5 exploration "Processes and I/O"
       if (currLine->background) {
-        printf("background pid is %d\n", getpid());
-        // need signal handler to immediately wait() for child processes to
-        // terminate
-        fflush(stdout);
+        // background without input or output should redirect to "/dev/null"
+        int devNull = open("/dev/null", O_WRONLY);
+        if (currLine->redirectInput == NULL) {
+          dup2(devNull, STDIN_FILENO);
+        }
+        if (currLine->redirectOutput == NULL) {
+          dup2(devNull, STDOUT_FILENO);
+        }
       }
-      // dup2 stuff?
       if (currLine->redirectInput) {
         // open source file
         int sourceFD = open(currLine->redirectInput, O_RDONLY);
@@ -229,26 +324,33 @@ void otherCommands(struct commandline *currLine) {
           exit(1);
         }
         // redirect FD 0 (stdin) to source file
-        int result = dup2(sourceFD, 0);
+        int result = dup2(sourceFD, STDIN_FILENO);
         if (result == -1) {
           perror("source dup2()");
-          exit(2);  // CHANGE THIS TO 1
+          printf("dup2() cannot redirect stdin to source file %s\n",
+                 currLine->redirectInput);
+          fflush(stdout);
+          exit(1);
         }
       }
       if (currLine->redirectOutput) {
         // output redirection
         int targetFD =
-            open(currLine->redirectOutput, O_WRONLY | O_CREAT | O_TRUNC,
-                 0644);  // 0640 or 0644?
+            open(currLine->redirectOutput, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        // 0640 or 0644?
         if (targetFD == -1) {
-          perror("target open()");
+          printf("cannot open %s for output\n", currLine->redirectOutput);
+          fflush(stdout);
           exit(1);
         }
         // redirect FD 1 (stdout) to targetFD
         int result = dup2(targetFD, 1);
         if (result == -1) {
           perror("dup2");
-          exit(2);  // CHANGE THIS TO 1
+          printf("dup2() cannot redirect stdout to target file %s\n",
+                 currLine->redirectOutput);
+          fflush(stdout);
+          exit(1);
         }
       }
       execvp(currLine->command, newargv);
@@ -267,19 +369,22 @@ void otherCommands(struct commandline *currLine) {
         // check if child process terminated normally
         if (WIFEXITED(childStatus)) {
           lastExitStatus = WEXITSTATUS(childStatus);
-          // printf("Child %d exited normally with status %d\n", childPid,
-          //       WEXITSTATUS(childStatus));
-          // fflush(stdout);
+          statusOrSignal = 0;
         } else {
           lastExitStatus = WTERMSIG(childStatus);
-          // printf("Child %d exited abnormally due to signal %d\n", childPid,
-          //       WTERMSIG(childStatus));
-          // fflush(stdout);
+          statusOrSignal = 1;
         }
       } else {
         // background process with WNOHANG
-        childPid = waitpid(childPid, &childStatus, WNOHANG);
+        // childPid = waitpid(childPid, &childStatus, WNOHANG);
+        waitpid(childPid, &childStatus, WNOHANG);
+        printf("background pid is %d\n", childPid);
+        fflush(stdout);
       }
+
+      // print background exit info if the returning pid is one of the stored
+      // background pids
+      // backgroundPidExit(&childStatus);
       break;
   }
 }
